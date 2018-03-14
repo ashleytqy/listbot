10df113c785d0abb51a29a2fd209f3fcdab72312
Ashley Teoh <atq203@nyu.edu>
2018-03-14 11:57:25 -0400

    x

---
+++ README.md
@@ -0,0 +1,30 @@
+# 📝 listbot 
+telegram list bot
+
+<img src="https://github.com/ashleytqy/listbot/blob/master/img/1.PNG" height="500px"/>
+<img src="https://github.com/ashleytqy/listbot/blob/master/img/2.PNG" height="500px"/>
+
+## 🤔 why listbot?
+I live in a dorm with four other suite mates and whenever we buy groceries for the suite, we would list down the things to get in our chat group. Whenever we want to add on to the list, we would have to copy the whole list and then add a new item on it. Sometimes, we might miss some items that were added while we were typing. So hopefully this bot helps with similar problem!
+
+
+## 🔧 functions
+- /start
+- /add [item]
+- /delete [index]
+- /peek
+- /clear
+ 
+## 🔑 getting API keys
+- [Telegram Bot key](https://core.telegram.org/bots#3-how-do-i-create-a-bot)
+  + update token information in `firebase.json`
+- [Firebase key](https://console.firebase.google.com/)
+  + update Firebase information in `app.js`
+
+## 🐎 run on local server
+```
+npm install
+```
+```
+node app.js
+```
--- app.js
+++ app.js
@@ -1,75 +1,115 @@
 var TelegramBot = require('node-telegram-bot-api');
+var token = 'TELEGRAM-API';
+var bot = new TelegramBot(token, {polling: true});
 var firebase = require("firebase");
 
-var token = '246332843:AAGEehkw7lLxaKp1yuBBney0HTu-hf67t4E';
-var bot = new TelegramBot(token, {polling: true});
+firebase.initializeApp({
+  serviceAccount: "firebase.json",
+  databaseURL: "https://DATABASE-URL.firebaseio.com"
+});
 
+var db = firebase.database();
+var listCollection = db.ref('server/data/');
 
-var list = [];
-//default listName
-var listName = "Groceries";
-var chatID = "";
-
+//BOT DIRECTIVES
 bot.onText(/\/start/, function(msg, match) {
-  chatID = msg.chat.id;
-  var user = msg.chat.first_name;
+  var chatID = msg.chat.id;
+  var name = msg.chat.first_name;
 
-  if (user == undefined) {
-    user = "all";
+  if (name == undefined) {
+    name = "all";
   }
-  output = "hey " + user + "! this is a list bot, of sorts. \
+
+  var output = "hey " + name + "! this is a list bot, of sorts. \
   to use this list bot, type /add [item], /delete [index of item], \
-  /clear, /changeListName [new list name], /peek";
+  /clear, /peek. \n\n please do not store important information in \
+  the lists -- data is not encrypted.";
   bot.sendMessage(chatID, output);
 });
 
+
 bot.onText(/\/add/, function(msg, match) {
-  chatID = msg.chat.id;
+  var chatID = msg.chat.id;
+
+  //getting the input item from user
   var item = match["input"];
-  list.push(item.substring(4));
-  bot.sendMessage(chatID, listToString());
+  item = item.substring(5);
+
+  var list = db.ref("server/data/chatIDs/" + chatID + "/list");
+  list.once("value", function(snapshot) {
+    var originalList = snapshot.val();
+    //in case the list has yet to be initialized
+    if (originalList == null) {
+      originalList = [];
+    }
+    originalList.push(item);
+    list.set(originalList);
+    printListInChat(chatID);
+  });
 });
+
 
 bot.onText(/\/delete/, function(msg, match) {
   chatID = msg.chat.id;
   var entry = Number(msg.text.substring(7));
-  if (entry > -1 && entry <= list.length) {
-    //offset zero-based index
-    list.splice(entry - 1, 1);
-  }
+  console.log(entry);
 
-  bot.sendMessage(chatID, listToString());
+  if (isNaN(entry)) {
+    bot.sendMessage(chatID, "Please enter a valid index!");
+  } else {
+      var list = db.ref("server/data/chatIDs/" + chatID + "/list");
+      list.once("value", function(snapshot) {
+
+        var originalList = snapshot.val();
+        //in case list has yet to be initialized
+        if (originalList == null) {
+          bot.sendMessage(chatID, "List empty now; nothing to delete!");
+        } else {
+          var length = originalList.length;
+          if (entry > -1 && entry <= length) {
+            //offset zero-based index
+            originalList.splice(entry - 1, 1);
+            console.log(originalList);
+            list.set(originalList);
+            printListInChat(chatID);
+          } else {
+            bot.sendMessage(chatID, "Please enter a valid index!");
+          }
+        }
+      });
+  }
 })
 
 bot.onText(/\/clear/, function(msg, match) {
-    chatID = msg.chat.id;
-    list = [];
-    bot.sendMessage(chatID, listName + " list emptied!");
+    var chatID = msg.chat.id;
+    var list = db.ref("server/data/chatIDs/" + chatID + "/list");
+    list.set([]);
+    bot.sendMessage(chatID, "List emptied!");
 });
 
-bot.onText(/\/peek/, function(msg, match) {
-  chatID = msg.chat.id;
-  bot.sendMessage(chatID, listToString());
-})
 
-bot.onText(/\/changeListName/, function(msg, match) {
-  listName = msg.text.substring(16);
-})
+bot.onText(/\/peek/, function(msg, match) {
+  var chatID = msg.chat.id;
+  printListInChat(chatID);
+});
 
 
 //HELPER FUNCTIONS
-function listToString() {
-    var output = "";
-    output += "\n" + listName + "\n";
-    output += "=======\n";
-    for (var i = 0; i < list.length; i ++) {
-      output += (i+1) + ". " + list[i] + "\n";
-    }
-    return output;
-}
+function printListInChat(userChatID) {
+  var list = db.ref("server/data/chatIDs/" + userChatID + "/list");
 
-function print(){
-  for (var i = 0; i < list.length; i++) {
-    console.log(idToList[chatID][i]);
-  }
+  list.once("value", function(snapshot) {
+    var output = "";
+    var data = snapshot.val();
+    if (data != null) {
+      output += "\n" + "* ~ List ~ *" + "\n";
+      output += "========\n";
+      for (var i = 0; i < data.length; i ++) {
+        output += (i+1) + ". " + data[i] + "\n";
+      };
+      bot.sendMessage(userChatID, output);
+    } else {
+      bot.sendMessage(userChatID, "List is empty!");
+    }
+  });
 }
